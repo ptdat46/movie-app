7331e8489bbe589a49483f36567b387d91ae1dc9
ptdat46 <ptdat46@gmail.com>
2024-05-22 15:44:34 +0700

    Pagination Genres

--- movie-app/src/pages/genres.jsx
+++ movie-app/src/pages/genres.jsx
@@ -14,57 +14,72 @@ function Genres() {
         active: '28',
     });
     const [moviesList, setMoviesList] = useState([]);
+    const [page, setPage] = useState(1);
     const handleGenre = (event) => {
         const clicked = event.target.id;
-        if(isActive.active === clicked) {
-            setActive({active: ''})
+        if (isActive.active === clicked) {
+            setActive({ active: '' })
         } else {
-            setActive({active: clicked});
+            setPage(1);
+            setActive({ active: clicked });
         }
     }
 
+    const handlePage = (page) => {
+        setPage(page);
+    }
+
     useEffect(() => {
-        axios.get(`/genres/${isActive.active}`)
-        .then(res => {
-            setMoviesList(res.data.results)
-        })
-        .catch(err => console.log(err))
-    }, [isActive])
+        axios.get(`/genres/${isActive.active}/${page}`)
+            .then(res => {
+                setMoviesList(res.data.results)
+            })
+            .catch(err => console.log(err))
+    }, [isActive, page])
 
     return (
         <div className="genres-main-div">
-            <Auth/>
-            <Navbar/>
+            <Auth />
+            <Navbar />
             <div className="genres-list text-white">
                 <div className="genres-group">
-                <div className="genre-item"><button id="28" className={isActive.active === '28' ? "active" : ""} onClick={(event) => handleGenre(event)}>Action</button></div>
-                <div className="genre-item"><button id="12" className={isActive.active === "12" ? "active" : ""} onClick={handleGenre}>Adventure</button></div>
-                <div className="genre-item"><button id="16" className={isActive.active === "16" ? "active" : ""} onClick={handleGenre}>Animation</button></div>
-                <div className="genre-item"><button id="35" className={isActive.active === "35" ? "active" : ""} onClick={handleGenre}>Comedy</button></div>
-                <div className="genre-item"><button id="80" className={isActive.active === "80" ? "active" : ""} onClick={handleGenre}>Crime</button></div>
-                <div className="genre-item"><button id="99" className={isActive.active === "99" ? "active" : ""} onClick={handleGenre}>Documentary</button></div>
-                <div className="genre-item"><button id="18" className={isActive.active === "18" ? "active" : ""} onClick={handleGenre}>Drama</button></div>
-                <div className="genre-item"><button id="10751" className={isActive.active === "10751" ? "active" : ""} onClick={handleGenre}>Family</button></div>
-                <div className="genre-item"><button id="14" className={isActive.active === "14" ? "active" : ""} onClick={handleGenre}>Fantasy</button></div>
+                    <div className="genre-item"><button id="28" className={isActive.active === '28' ? "active" : ""} onClick={(event) => handleGenre(event)}>Action</button></div>
+                    <div className="genre-item"><button id="12" className={isActive.active === "12" ? "active" : ""} onClick={handleGenre}>Adventure</button></div>
+                    <div className="genre-item"><button id="16" className={isActive.active === "16" ? "active" : ""} onClick={handleGenre}>Animation</button></div>
+                    <div className="genre-item"><button id="35" className={isActive.active === "35" ? "active" : ""} onClick={handleGenre}>Comedy</button></div>
+                    <div className="genre-item"><button id="80" className={isActive.active === "80" ? "active" : ""} onClick={handleGenre}>Crime</button></div>
+                    <div className="genre-item"><button id="99" className={isActive.active === "99" ? "active" : ""} onClick={handleGenre}>Documentary</button></div>
+                    <div className="genre-item"><button id="18" className={isActive.active === "18" ? "active" : ""} onClick={handleGenre}>Drama</button></div>
+                    <div className="genre-item"><button id="10751" className={isActive.active === "10751" ? "active" : ""} onClick={handleGenre}>Family</button></div>
+                    <div className="genre-item"><button id="14" className={isActive.active === "14" ? "active" : ""} onClick={handleGenre}>Fantasy</button></div>
                 </div>
                 <div className="genres-group">
-                <div className="genre-item"><button id="36" className={isActive.active === "36" ? "active" : ""} onClick={handleGenre}>History</button></div>
-                <div className="genre-item"><button id="27" className={isActive.active === "27" ? "active" : ""} onClick={handleGenre}>Horror</button></div>
-                <div className="genre-item"><button id="10402" className={isActive.active === "10402" ? "active" : ""} onClick={handleGenre}>Music</button></div>
-                <div className="genre-item"><button id="9648" className={isActive.active === "9648" ? "active" : ""} onClick={handleGenre}>Mystery</button></div>
-                <div className="genre-item"><button id="10749" className={isActive.active === "10749" ? "active" : ""} onClick={handleGenre}>Romance</button></div>
-                <div className="genre-item"><button id="878" className={isActive.active === "878" ? "active" : ""} onClick={handleGenre}>Science Fiction</button></div>
+                    <div className="genre-item"><button id="36" className={isActive.active === "36" ? "active" : ""} onClick={handleGenre}>History</button></div>
+                    <div className="genre-item"><button id="27" className={isActive.active === "27" ? "active" : ""} onClick={handleGenre}>Horror</button></div>
+                    <div className="genre-item"><button id="10402" className={isActive.active === "10402" ? "active" : ""} onClick={handleGenre}>Music</button></div>
+                    <div className="genre-item"><button id="9648" className={isActive.active === "9648" ? "active" : ""} onClick={handleGenre}>Mystery</button></div>
+                    <div className="genre-item"><button id="10749" className={isActive.active === "10749" ? "active" : ""} onClick={handleGenre}>Romance</button></div>
+                    <div className="genre-item"><button id="878" className={isActive.active === "878" ? "active" : ""} onClick={handleGenre}>Science Fiction</button></div>
                 </div>
                 <div className="genres-group">
-                <div className="genre-item"><button id="10770" className={isActive.active === "10770" ? "active" : ""} onClick={handleGenre}>TV Movie</button></div>
-                <div className="genre-item"><button id="53" className={isActive.active === "53" ? "active" : ""} onClick={handleGenre}>Thriller</button></div>
-                <div className="genre-item"><button id="10752" className={isActive.active === "10752" ? "active" : ""} onClick={handleGenre}>War</button></div>
-                <div className="genre-item"><button id="37" className={isActive.active === "37" ? "active" : ""} onClick={handleGenre}>Western</button></div>
+                    <div className="genre-item"><button id="10770" className={isActive.active === "10770" ? "active" : ""} onClick={handleGenre}>TV Movie</button></div>
+                    <div className="genre-item"><button id="53" className={isActive.active === "53" ? "active" : ""} onClick={handleGenre}>Thriller</button></div>
+                    <div className="genre-item"><button id="10752" className={isActive.active === "10752" ? "active" : ""} onClick={handleGenre}>War</button></div>
+                    <div className="genre-item"><button id="37" className={isActive.active === "37" ? "active" : ""} onClick={handleGenre}>Western</button></div>
                 </div>
             </div>
             {!!moviesList &&
-                <MoviesList list = {moviesList.slice(0, 40)}/>
+                <MoviesList list={moviesList.slice(0, 40)} />
             }
+            <nav>
+                <ul class="pagination">
+                    <li className={page == 1 ? "page-item active" : "page-item"}><a className="page-link" href="#" onClick={() => handlePage(1)}>1</a></li>
+                    <li className={page == 2 ? "page-item active" : "page-item"}><a className="page-link" href="#" onClick={() => handlePage(2)}>2</a></li>
+                    <li className={page == 3 ? "page-item active" : "page-item"}><a className="page-link" href="#" onClick={() => handlePage(3)}>3</a></li>
+                    <li className={page == 4 ? "page-item active" : "page-item"}><a className="page-link" href="#" onClick={() => handlePage(4)}>4</a></li>
+                    <li className={page == 5 ? "page-item active" : "page-item"}><a className="page-link" href="#" onClick={() => handlePage(5)}>5</a></li>
+                </ul>
+            </nav>
         </div>
     )
 }
--- server/routes/genres.js
+++ server/routes/genres.js
@@ -2,9 +2,10 @@ const express = require("express");
 const router = express.Router();
 const axios = require('axios');
 
-router.get('/:genreId', async (req, res) => {
+router.get('/:genreId/:page', async (req, res) => {
     const genreId = req.params.genreId
-    const data = await axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=e9e9d8da18ae29fc430845952232787c&include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&with_genres=${genreId}`)
+    const page = req.params.page;
+    const data = await axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=e9e9d8da18ae29fc430845952232787c&include_adult=false&include_video=false&language=en-US&page=${page}&sort_by=popularity.desc&with_genres=${genreId}`)
     res.json(data.data);
 })
 
